46c31b4056b75d39697c1911df4c76a05c55cf55
john1l <94162057+john1l@users.noreply.github.com>
2022-01-29 13:42:47 +0900

    Time: 115 ms (5.51%), Space: 13.9 MB (99.69%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -2,16 +2,14 @@ class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
         
         
-        for i in range(0, len(prices)-1):
-            for j in range(i+1, len(prices)):
-                
-                if (prices[i] >=prices[j]):
-                    prices[i] -= prices[j]
-                    break 
-                    
+        
+        stack = [] 
+        for i , p in enumerate(prices):
+            while stack and prices[stack[-1]] >= p:
+                    prices[stack.pop( )] -=p 
+            stack.append(i)
         return prices 
-                
-            
+        
             
         
         # cannot use greedy, unless using -> O(n^2)
