a1373a7a38014bbc0e65ccdc2c8b5e6a442ea408
john1l <94162057+john1l@users.noreply.github.com>
2022-02-01 14:24:51 +0900

    Time: 48 ms (36.41%), Space: 13.7 MB (99.85%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -3,10 +3,7 @@ class Solution:
         
         pre = 0
         maxv = 0 
-        ret = [0]
         for i in range(len(gain)):
             pre += gain[i]
             maxv = max(maxv , pre)
-
-        #et.append(pre)
         return  maxv
