8fba0c3aec8dc7b86c7877a24021a016e94dc28e
john1l <94162057+john1l@users.noreply.github.com>
2022-02-07 14:37:20 +0900

    Time: 70 ms (11.54%), Space: 13.9 MB (88.02%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        
+        
+        ch = 0 
+        
+        for char_ in s:
+            ch ^= ord(char_)
+            
+        for char_ in t:
+            ch ^= ord(char_)
+            
+        return chr(ch)
