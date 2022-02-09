e2de8bb1b160ca81fe599abdc4ac97b822fc951c
john1l <94162057+john1l@users.noreply.github.com>
2022-02-09 16:51:27 +0900

    Time: 113 ms (46.71%), Space: 15.6 MB (84.25%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.py
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.py
@@ -7,7 +7,7 @@ class Solution:
         
         for key in counter:
             
-            if k>0 and key+k in counter:
+            if k>0 and key-k in counter:
                 res +=1
             elif k==0 and counter[key] >1:
                 res +=1 
