d839bd6621e730b43ef178b4c3baa813eb10fb7c
john1l <94162057+john1l@users.noreply.github.com>
2022-02-15 10:34:51 +0900

    Time: 57 ms (61.82%), Space: 13.9 MB (74.22%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.py
+++ 832-flipping-an-image/832-flipping-an-image.py
@@ -1,9 +1,9 @@
 class Solution:
     def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+                
             
-            def converter(nums):
-                 return [ 1^n for n in nums]
-            ml = [] 
-            for im in image:
-                ml.append( converter( im[::-1] ))
-            return ml 
+            return [[ 1^x for x in row[::-1]]   for row in image ]
+        
+        
+        
+        
