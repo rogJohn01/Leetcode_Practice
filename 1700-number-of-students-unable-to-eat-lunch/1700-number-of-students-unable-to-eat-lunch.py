875a67735c69cff4b5d86bf342cb2a260a26015d
john1l <94162057+john1l@users.noreply.github.com>
2022-02-15 09:24:05 +0900

    Time: 96 ms (5.16%), Space: 13.9 MB (76.05%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        
+            from collections import deque 
+            stud = deque(students)
+            sand = deque(sandwiches)
+
+            cnt = 0 
+            left = 0
+            while left < len(stud): 
+
+                if stud[left] == sand[0]:
+                    stud.popleft()
+                    sand.popleft()
+
+                elif sand[0] not in stud:
+                    return len(stud)
+
+
+                else: 
+                    stud.append( stud[0])
+                    stud.popleft()
+
+            return 0 
