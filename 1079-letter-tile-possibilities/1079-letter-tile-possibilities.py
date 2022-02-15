cead0c4fa56c9a6b5ab6280425fec63dba3e552f
john1l <94162057+john1l@users.noreply.github.com>
2022-02-15 14:48:11 +0900

    Time: 588 ms (7.89%), Space: 19.4 MB (35.88%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        cnt =0 
+        ret = []
+        def dfs(path, cntr ,k):
+            global cnt 
+
+            if len(path) ==k: 
+                ret.append(''.join(path))
+
+
+            for c in cntr: 
+
+                if cntr[c] >=1:
+                    cntr[c] -=1 
+                    dfs(path+[c] , cntr ,k)
+                    cntr[c] +=1 
+
+        for k in range(1, len(tiles)+1):
+
+            dfs( [] ,Counter(tiles) ,k)
+        return len(ret)
