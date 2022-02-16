f548d3377c5af6055116d18f42848d5a7b4f3af1
john1l <94162057+john1l@users.noreply.github.com>
2022-02-16 09:34:58 +0900

    Time: 171 ms (10.74%), Space: 14.2 MB (73.16%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -2,19 +2,17 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
         
-        stack = [] 
         al = [] 
         for n1 in nums1:
             
-            stack.append(n1)
             mode = False 
             mode2 = True 
-            for i, n2 in enumerate(nums2):
+            for n2 in nums2:
                 
-                if stack[-1] == n2:  
+                if n1 == n2:  
                     mode =True 
                     
-                if mode and stack[-1] < n2:
+                if mode and n1 < n2:
                     al.append(n2)
                     mode2 = False 
                     break 
