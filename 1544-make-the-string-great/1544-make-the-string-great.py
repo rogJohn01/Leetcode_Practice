67b7e961b291124f7307563cbb8a96e0b5a247b4
john1l <94162057+john1l@users.noreply.github.com>
2022-02-16 10:16:06 +0900

    Time: 48 ms (52.29%), Space: 14 MB (70.80%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -0,0 +1,14 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        
+        
+        stack = []
+        stack.append(s[0])
+        for i in range(1,len(s)):
+            
+            if  stack and stack[-1].swapcase() == s[i]:
+                stack.pop()
+            
+            else:
+                stack.append(s[i])
+        return ''.join(stack)
