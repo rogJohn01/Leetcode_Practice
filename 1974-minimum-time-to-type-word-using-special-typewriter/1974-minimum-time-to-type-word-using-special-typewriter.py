eb48069c7e18cbb95a300cb32b66d15e1244b915
john1l <94162057+john1l@users.noreply.github.com>
2022-02-18 10:33:26 +0900

    Time: 56 ms (23.52%), Space: 13.9 MB (85.79%) - LeetHub

--- 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.py
+++ 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.py
@@ -1,22 +1,11 @@
 class Solution:
     def minTimeToType(self, word: str) -> int:
-            al = [] 
-            alp = 'abcdefghijklmnopqrstuvwxyz'
-            for i in range(len(word)):
-
-                    if i ==0:
-                        k1 = abs( alp.index(word[i]) - alp.index('a') )
-                        k2 = abs( - alp.index(word[i]) + alp.index('a') )
-                        if k2 >0:
-                            k2 = 26 -k2 
-                        ans = min( k1 , k2) 
-                        al.append(ans )
-                    else: 
-                        k1 = abs( alp.index(word[i]) - alp.index(word[i-1]) )
-                        k2 = abs( - alp.index(word[i]) + alp.index(word[i-1]) )
-                        if k2 >0:
-                            k2 = 26 -k2 
-                        ans = min( k1 , k2) 
-                        al.append(ans )
-
-            return   sum(al) + len(al)
+          
+            
+        ans = len(word)
+        prev = 'a'
+        for w in word:
+            val = (ord(w) - ord(prev)) % 26
+            ans += min(val , 26-val )
+            prev = w 
+        return ans 
