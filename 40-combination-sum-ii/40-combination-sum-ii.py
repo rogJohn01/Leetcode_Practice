53d4883dcadcb3dafe04dafacef18af789999ff9
john1l <94162057+john1l@users.noreply.github.com>
2022-02-18 14:41:29 +0900

    Time: 72 ms (75.17%), Space: 13.9 MB (98.07%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -1,31 +1,23 @@
 class Solution:
     def combinationSum2(self, cand: List[int], target: int) -> List[List[int]]:
             
+        cand.sort()
+        ret = [] 
+        self.backtrack( cand , [] , ret , target , 0)
+        return ret 
+    
+    
+    def backtrack(self, nums , path , ret , target ,start):
+        
+        if not target:
+            ret.append(path)
+            return 
+        
+        for i in range(start , len(nums)):
             
-            if sum(cand) < target:
-                return 
-            if len(cand) >10 and len( Counter(cand).keys()) ==1 and sum(cand) >= target:
-                return [list(Counter(cand).keys())*(target//cand[0])]
+            if i > start and nums[i] ==nums[i-1]:
+                continue 
+            if nums[i] > target:
+                break 
             
-            
-            ret = []
-            def backtrack( rem , path , cntr, start):
-
-
-                if rem ==0:
-                    path.sort()
-                    if path not in ret:
-                        ret.append(path[:])
-
-                if rem < 0:
-                    return 
-
-                for i in range(start , len(cand)):
-                    if cntr[cand[i]] >0 :
-                        cntr[cand[i]] -=1
-                        backtrack(rem -cand[i] , path+[cand[i]] , cntr, i)
-                        cntr[cand[i]] +=1
-
-
-            backtrack(target , [] , Counter(cand)  ,0  )
-            return ret 
+            self.backtrack(nums,path+[nums[i]] , ret , target - nums[i] , i+1 )
