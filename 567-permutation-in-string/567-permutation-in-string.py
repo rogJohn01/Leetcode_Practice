ae6f3edca3d656ce84570fbb4e731092e4335da8
john1l <94162057+john1l@users.noreply.github.com>
2022-02-21 20:23:27 +0900

    Time: 91 ms (73.25%), Space: 14 MB (81.13%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,21 +1,27 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         
-        
-        A = [ord(x) - ord('a') for x in s1]
-        B = [ord(x) - ord('a') for x in s2]
-        
-        target = [0] * 26
-        for x in A:
-            target[x] +=1 
-        
-        window = [0]*26
-        for i, x in enumerate(B):
-            
-            window[x] +=1 
-            if i >= len(A):
-                window[B[i-len(A)]] -=1 
-            if window ==target:
+
+
+        ls1 = len(s1)
+        ls2 = len(s2)
+
+        s1alp = 26*[0]
+        s2alp = 26*[0]
+
+
+        for s in s1:
+            s1alp[ord(s)- ord('a')] +=1 
+
+        for i in range(ls2):
+
+            s2alp[ord(s2[i]) - ord('a')] +=1
+
+            if i>=ls1:
+                s2alp[ord(s2[i-ls1]) - ord('a')] -=1 
+            if s1alp ==s2alp:
                 return True 
-        return False 
-        
+        return False  
+
+
+    
