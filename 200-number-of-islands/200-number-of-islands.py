26970955acbd566fb88beb8ad8e43d8ab841fc12
john1l <94162057+john1l@users.noreply.github.com>
2022-02-21 16:05:43 +0900

    Time: 520 ms (29.01%), Space: 16.2 MB (99.12%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,24 +1,38 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        cnt = 0 
-        def dfs(i,j):
-            
-            if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j] !='1':
-                return 
-            
-            grid[i][j] =0 
-            
-            dfs(i+1 , j)
-            dfs(i-1 , j)
-            dfs(i , j+1)
-            dfs(i , j-1 )
-            
-            
-            
-        for i in range(len(grid)):
-                for j in range(len(grid[0])):
-                    if grid[i][j] =='1':
-                        dfs(i,j)
-                        cnt +=1 
-        return cnt 
+        if not grid or not grid[0]:
+            return 0
+
+        m, n = len(grid), len(grid[0])
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    self.bfs(grid, i, j)
+                    count += 1
+        return count
+    
+    def is_valid(self, grid,x,y):
+        m, n = len(grid) , len(grid[0])
+        if x < 0 or y < 0 or x>= m or y >= n:
+            return False
+        return True 
+    
+    def bfs(self,grid ,x,y):
+        
+        queue = deque()
+        queue.append((x,y))
+        grid[x][y] ='0'
+        
+        while queue:
+            x, y = queue.popleft()
+            direct = [(0,1),(0,-1), (-1,0) , (1,0) ] 
+            for d in direct:
+                nx , ny = x+d[0] , y+d[1]
+                
+                if self.is_valid(grid, nx,ny) and grid[nx][ny] =='1':
+                    queue.append((nx,ny))
+                    grid[nx][ny] = '0'
+                             
+                             
