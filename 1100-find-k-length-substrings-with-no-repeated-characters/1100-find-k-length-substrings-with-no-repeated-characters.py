89ce07e304ac3f92c4b98eaebdf32d2ab053e361
john1l <94162057+john1l@users.noreply.github.com>
2022-02-21 16:12:48 +0900

    Time: 67 ms (64.05%), Space: 14.1 MB (17.60%) - LeetHub

--- 1100-find-k-length-substrings-with-no-repeated-characters/1100-find-k-length-substrings-with-no-repeated-characters.py
+++ 1100-find-k-length-substrings-with-no-repeated-characters/1100-find-k-length-substrings-with-no-repeated-characters.py
@@ -1,27 +1,45 @@
 
 class Solution:
     def numKLenSubstrNoRepeats(self, s: str, k: int) -> int:
+        # We can reuse the condition from the first approach
+        # as for k > 26, there can be no substrings with only unique characters
         if k > 26:
             return 0
         answer = 0
         n = len(s)
         
-        for i in range(n - k + 1):
-             # Initializing an empty frequency array
-            freq = [0] * 26
+        # Initializing the left and right pointers
+        left = right = 0
+        
+        # Initializing an empty frequency array
+        freq = [0] * 26
+        
+        # Function to obtain the index of a character according to the alphabet
+        def get_val(ch: str) -> int:
+            return ord(ch) - ord('a')
+        
+        while right < n:
             
-            for j in range(i, i + k):
-                curr_char = ord(s[j]) - ord('a')
-                
-                # Incrementing the frequency of current character
-                freq[curr_char] += 1
-                
-                # If a repeated character is found, we stop the loop
-                if freq[curr_char] > 1:
-                    break
-            else:
-                # If the substring does not have any repeated characters,
-                # we increment the answer
+            # Add the current character in the frequency array
+            freq[get_val(s[right])] += 1
+            
+            # If the current character appears more than once in the frequency array
+            # keep contracting the window and removing characters from the
+            # frequency array till the frequency of the current character becomes 1.
+            while freq[get_val(s[right])] > 1:
+                freq[get_val(s[left])] -= 1
+                left += 1
+            
+            # Check if the length of the current unique substring is equal to k
+            if right - left + 1 == k:
                 answer += 1
+                
+                # Contract the window and remove the leftmost character from the
+                # frequency array
+                freq[get_val(s[left])] -= 1
+                left += 1
+            
+            # Expand the window
+            right += 1
         
         return answer
