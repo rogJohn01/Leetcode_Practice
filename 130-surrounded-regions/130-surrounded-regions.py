407c0d02e425c1b7012da7cb8a56357f2913afaa
john1l <94162057+john1l@users.noreply.github.com>
2022-02-23 15:27:41 +0900

    Time: 144 ms (82.79%), Space: 15.4 MB (86.05%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.py
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -1,36 +1,19 @@
 class Solution(object):
     def solve(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: None Do not return anything, modify board in-place instead.
-        """
-        if not board or not board[0]:
-            return
-
-        self.ROWS = len(board)
-        self.COLS = len(board[0])
-
-        # Step 1). retrieve all border cells
-        from itertools import product
-        borders = list(product(range(self.ROWS), [0, self.COLS-1])) \
-                + list(product([0, self.ROWS-1], range(self.COLS)))
-
-        # Step 2). mark the "escaped" cells, with any placeholder, e.g. 'E'
-        for row, col in borders:
-            self.DFS(board, row, col)
-
-        # Step 3). flip the captured cells ('O'->'X') and the escaped one ('E'->'O')
-        for r in range(self.ROWS):
-            for c in range(self.COLS):
-                if board[r][c] == 'O':   board[r][c] = 'X'  # captured
-                elif board[r][c] == 'E': board[r][c] = 'O'  # escaped
-
-
-    def DFS(self, board, row, col):
-        if board[row][col] != 'O':
-            return
-        board[row][col] = 'E'
-        if col < self.COLS-1: self.DFS(board, row, col+1)
-        if row < self.ROWS-1: self.DFS(board, row+1, col)
-        if col > 0: self.DFS(board, row, col-1)
-        if row > 0: self.DFS(board, row-1, col)
+        queue = collections.deque([])
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if (r in [0, len(board)-1] or c in [0, len(board[0])-1]) and board[r][c] == "O":
+                    queue.append((r, c))
+        while queue:
+            r, c = queue.popleft()
+            if 0<=r<len(board) and 0<=c<len(board[0]) and board[r][c] == "O":
+                board[r][c] = "."
+                queue.extend([(r-1, c),(r+1, c),(r, c-1),(r, c+1)])
+        
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == ".":
+                    board[r][c] = "O"
