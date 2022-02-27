262c32893c0a2873797005f1dd7e38f9817b0e77
john1l <94162057+john1l@users.noreply.github.com>
2022-02-27 15:22:31 +0900

    Time: 592 ms (17.83%), Space: 16.8 MB (81.45%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.py
+++ 286-walls-and-gates/286-walls-and-gates.py
@@ -4,24 +4,26 @@ class Solution:
         Do not return anything, modify rooms in-place instead.
         """
 
-        R, C = len(rooms), len(rooms[0])
+
+
+        R = len(rooms) ; C = len(rooms[0])
 
         for r in range(R):
             for c in range(C):
                 if rooms[r][c] ==0:
                     queue = deque()
                     for a,b in [(1,0),(-1,0),(0,1),(0,-1)]:
-                        queue.append((r+a,c+b ,1))
+                        queue.append((r+a,c+b,1))
 
-                        visited = set()
-
+                        visited = set() 
                         while queue:
-                            x, y , v = queue.popleft()
-                            if x <0 or x>= R or y<0 or y>=C:
+                            x,y ,v = queue.popleft()
+
+                            if x <0 or x >=R or y < 0 or y >=C:
                                 continue 
-                            if rooms[x][y] in [0,-1] or (x,y) in visited: 
+                            if rooms[x][y] in [-1, 0] or (x,y) in visited:
                                 continue 
                             visited.add((x,y))
-                            rooms[x][y] = min(rooms[x][y] ,v)
+                            rooms[x][y] = min( rooms[x][y] , v)
                             for a,b in [(1,0),(-1,0),(0,1),(0,-1)]:
-                                queue.append((x+a, y+b , v+1 ))
+                                queue.append((x+a,y+b , v+1))
