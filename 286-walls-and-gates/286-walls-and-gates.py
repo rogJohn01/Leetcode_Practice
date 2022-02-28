8fa0928e0db18a0d56a2e6e5f0ad23c7109169d6
john1l <94162057+john1l@users.noreply.github.com>
2022-02-28 13:53:46 +0900

    Time: 597 ms (17.59%), Space: 16.8 MB (88.02%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.py
+++ 286-walls-and-gates/286-walls-and-gates.py
@@ -5,25 +5,28 @@ class Solution:
         """
 
 
-
         R = len(rooms) ; C = len(rooms[0])
 
         for r in range(R):
             for c in range(C):
-                if rooms[r][c] ==0:
+
+                if rooms[r][c]==0:
+
                     queue = deque()
                     for a,b in [(1,0),(-1,0),(0,1),(0,-1)]:
                         queue.append((r+a,c+b,1))
 
-                        visited = set() 
+                        visited = set()
                         while queue:
-                            x,y ,v = queue.popleft()
+                            x,y,v = queue.popleft()
 
-                            if x <0 or x >=R or y < 0 or y >=C:
+                            if x < 0 or x >= R or y <0 or y >=C:
                                 continue 
-                            if rooms[x][y] in [-1, 0] or (x,y) in visited:
+                            if rooms[x][y] in [-1,0] or (x,y) in visited:
                                 continue 
+
                             visited.add((x,y))
-                            rooms[x][y] = min( rooms[x][y] , v)
+                            rooms[x][y] = min(rooms[x][y] , v)
                             for a,b in [(1,0),(-1,0),(0,1),(0,-1)]:
-                                queue.append((x+a,y+b , v+1))
+                                queue.append((x+a,y+b,v+1))
+
