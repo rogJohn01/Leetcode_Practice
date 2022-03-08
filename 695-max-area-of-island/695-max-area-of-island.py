02e1b63a7ae22d13dc33a9abc93b6dcf3612985e
john1l <94162057+john1l@users.noreply.github.com>
2022-03-08 14:43:18 +0900

    Time: 211 ms (44.23%), Space: 14.1 MB (94.59%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,26 +1,35 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         
-        
-        global al 
-        def dfs(x,y):
-            global al 
-            if x < 0 or x>=len(grid) or y <0 or y>= len(grid[0]) or grid[x][y]!=1:
-                return 
-            
+        from collections import deque 
+
+      
+        def bfs(x,y ):
+
+            queue = deque()
+            queue.append((x,y))
+            dx = [1,-1,0,0] 
+            dy = [0,0,1,-1]
             grid[x][y] ='#'
-            
-            al +=1 
-            dfs(x+1,y)
-            dfs(x-1,y)
-            dfs(x,y+1)
-            dfs(x,y-1)
-            
-        ret = [] 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] ==1:
-                    al = 0
-                    dfs(i,j )
-                    ret.append(al)
-        return max(ret) if ret else  0
+            cnt = 0 
+            while queue:
+                x, y  = queue.popleft()
+                cnt +=1 
+                for i in range(4):
+                    nx = x +dx[i]
+                    ny = y +dy[i]
+                    if nx <0 or nx >=R or ny<0 or ny>=C or grid[nx][ny] !=1:
+                        continue 
+                    if grid[nx][ny] ==1:
+                        queue.append((nx,ny))
+                        grid[nx][ny] ='#'
+            return cnt 
+
+        R = len(grid) ; C = len(grid[0])
+        ans =0 
+        for r  in range(R):
+            for c in range(C):
+                if grid[r][c] ==1:
+                    ans = max(ans, bfs(r,c))
+
+        return ans 
