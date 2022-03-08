6010697f60f15baa4783c420bb736a0d1a615994
john1l <94162057+john1l@users.noreply.github.com>
2022-03-09 08:10:33 +0900

    Time: 192 ms (91.74%), Space: 15 MB (17.52%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -2,15 +2,19 @@ class Solution:
     def findCircleNum(self, A: List[List[int]]) -> int:
         N = len(A)
         seen = set()
-        def dfs(node):
-            for nei, adj in enumerate(A[node]):
-                if adj and nei not in seen:
-                    seen.add(nei)
-                    dfs(nei)
 
-        ans = 0
+        def dfs(node):
+
+            for i , v in enumerate(A[node]):
+                if v and i not in seen:
+                    seen.add(i)
+                    dfs(i)
+
+
+
+        ans = 0 
         for i in range(N):
             if i not in seen:
                 dfs(i)
-                ans += 1
-        return ans
+                ans +=1 
+        return ans 
