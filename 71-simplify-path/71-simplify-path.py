0e10b6ea7ae469e454a7e0f06c0e4852fda7a70d
john1l <94162057+john1l@users.noreply.github.com>
2022-03-15 10:13:55 +0900

    Time: 40 ms (70.35%), Space: 14 MB (56.52%) - LeetHub

--- 71-simplify-path/71-simplify-path.py
+++ 71-simplify-path/71-simplify-path.py
@@ -1,26 +1,17 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
 
-        # Initialize a stack
-        stack = []
 
-        # Split the input string on "/" as the delimiter
-        # and process each portion one by one
-        for portion in path.split("/"):
+        st = [] 
+        for port in path.split('/'):
 
-            # If the current component is a "..", then
-            # we pop an entry from the stack if it's non-empty
-            if portion == "..":
-                if stack:
-                    stack.pop()
-            elif portion == "." or not portion:
-                # A no-op for a "." or an empty string
-                continue
+            if port == '..':
+                if st:
+                    st.pop()
+
+            elif port == '.' or not port:
+                continue 
+
             else:
-                # Finally, a legitimate directory name, so we add it
-                # to our stack
-                stack.append(portion)
-
-        # Stich together all the directory names together
-        final_str = "/" + "/".join(stack)
-        return final_str
+                st.append(port)
+        return '/'+'/'.join(st)
