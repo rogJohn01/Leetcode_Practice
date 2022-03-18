fa00e7b877c96aa1d8d6e1a8736566991aea9120
john1l <94162057+john1l@users.noreply.github.com>
2022-03-18 20:36:34 +0900

    Time: 91 ms (51.04%), Space: 13.8 MB (71.29%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,17 +1,4 @@
-class Solution {
-public:
-    bool isPalindrome(int x) {
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
         
-        if (x<0 || (x!=0 && x%10 ==0 )) return false; 
-        
-        int sum =0 ; 
-        while ( x>sum ){
-            
-            sum = sum*10 + x%10;
-            x  /= 10; 
-        }
-        
-        return (x==sum) || (x ==sum/10);
-        
-    }
-};
+             return str(x) ==str(x)[::-1]
