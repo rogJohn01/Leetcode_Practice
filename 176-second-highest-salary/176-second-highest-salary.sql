5cde3084c2269a3e0bbaa117045e780b59cf555e
john1l <94162057+john1l@users.noreply.github.com>
2022-03-23 19:55:05 +0900

    Time: 174 ms (93.63%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,8 +1,7 @@
-SELECT
-    (SELECT DISTINCT
-            Salary
-        FROM
-            Employee
-        ORDER BY Salary DESC
-        LIMIT 1 OFFSET 1) AS SecondHighestSalary
-;
+SELECT 
+    IFNULL( 
+    (SELECT DISTINCT Salary 
+    FROM Employee 
+    ORDER BY Salary DESC 
+    LIMIT 1 OFFSET 1),
+    NULL) AS SecondHighestSalary 
