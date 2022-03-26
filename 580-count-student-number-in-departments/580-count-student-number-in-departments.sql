0ef5420dd9a83cb23ab2463ed12f3de5e9f6f292
john1l <94162057+john1l@users.noreply.github.com>
2022-03-26 17:29:02 +0900

    Time: 585 ms (99.14%), Space: 0B (100.00%) - LeetHub

--- 580-count-student-number-in-departments/580-count-student-number-in-departments.sql
+++ 580-count-student-number-in-departments/580-count-student-number-in-departments.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 
 
-select d.dept_name , ifnull( count(s.student_id) ,0) as student_number from student s right  join department d 
+select d.dept_name ,  count(s.student_id) as student_number from student s right  join department d 
 on d.dept_id = s.dept_id 
 group by d.dept_name 
 order by student_number desc  , d.dept_name 
