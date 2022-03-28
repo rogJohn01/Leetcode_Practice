6f4a4411be03b61b738cfd0ef131e29dc5dd0846
john1l <94162057+john1l@users.noreply.github.com>
2022-03-28 16:41:19 +0900

    Time: 631 ms (46.31%), Space: 14.9 MB (91.39%) - LeetHub

--- 531-lonely-pixel-i/531-lonely-pixel-i.py
+++ 531-lonely-pixel-i/531-lonely-pixel-i.py
@@ -1,20 +1,17 @@
 class Solution:
-    def findLonelyPixel(self, mat: List[List[str]]) -> int:
+    def findLonelyPixel(self, picture: List[List[str]]) -> int:
         
         
-
-        pix = 0 
-        loco = [] ; R =len(mat) ; C =len(mat[0])
-        for r in range(R):
-            for c in range(C):
-                if mat[r][c] =='B':
-                    lonely = True 
-                    for r1 in range(R):
-                        if r1 !=r and mat[r1][c] =='B':
-                            lonely = False 
-                    for c1 in range(C):
-                        if c1 !=c and  mat[r][c1] == 'B':
-                            lonely = False 
-                    if lonely:
-                        pix +=1 
-        return pix 
+        n,m=len(picture),len(picture[0])
+        res=0
+        for j in range(m):
+            found=False
+            for i in range(n):
+                if picture[i][j]=='B':
+                    if found or picture[i].count('B')!=1:
+                        found=False
+                        break
+                    found=True
+            if found:
+                res+=1
+        return res
