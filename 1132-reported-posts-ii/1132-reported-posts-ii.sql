e5cb2609903c29ac9557f7656c6578dab66c5e3a
john1l <94162057+john1l@users.noreply.github.com>
2022-03-28 13:19:12 +0900

    Time: 1085 ms (32.59%), Space: 0B (100.00%) - LeetHub

---
+++ 1132-reported-posts-ii/1132-reported-posts-ii.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+select round(sum(percent)/count(distinct action_date),2) as average_daily_percent
+from
+    (select a.action_date,
+    count(distinct r.post_id)/count(distinct a.post_id)*100 as percent
+    from actions a left join removals r
+    on a.post_id = r.post_id
+    where a.extra='spam'
+    group by 1) temp;
