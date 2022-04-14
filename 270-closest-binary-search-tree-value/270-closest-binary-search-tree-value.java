5a8f91227ad2725ae2ce2766c4e74cd5b666d68d
john1l <94162057+john1l@users.noreply.github.com>
2022-04-14 15:04:57 +0900

    Time: 0 ms (100.00%), Space: 42.1 MB (90.69%) - LeetHub

---
+++ 270-closest-binary-search-tree-value/270-closest-binary-search-tree-value.java
@@ -0,0 +1,11 @@
+class Solution {
+  public int closestValue(TreeNode root, double target) {
+    int val, closest = root.val;
+    while (root != null) {
+      val = root.val;
+      closest = Math.abs(val - target) < Math.abs(closest - target) ? val : closest;
+      root =  target < root.val ? root.left : root.right;
+    }
+    return closest;
+  }
+}
