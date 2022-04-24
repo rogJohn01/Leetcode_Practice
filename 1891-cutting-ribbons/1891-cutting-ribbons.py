629039756f6a9f8d95428a97795310146ba4b6e9
john1l <94162057+john1l@users.noreply.github.com>
2022-04-24 17:30:01 +0900

    Time: 2733 ms (11.17%), Space: 27.7 MB (74.07%) - LeetHub

--- 1891-cutting-ribbons/1891-cutting-ribbons.py
+++ 1891-cutting-ribbons/1891-cutting-ribbons.py
@@ -3,7 +3,6 @@ class Solution:
         
 
         l = 1 ; r = max(ribbons)  
-        maxv = 0 
         while l <=r: 
 
             m = (l+r) >>1 
@@ -12,26 +11,12 @@ class Solution:
             for rib in ribbons: 
                 cnt += rib//m 
 
+
+
             if cnt >= k: 
                 l = m +1  
-                maxv = max( maxv , m ) 
             else: 
                 r = m-1 
 
-        return maxv  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return  r    
 
