3955a1c8e8b1b6e61e382d2cc6b882c8c4b3c6e9
john1l <94162057+john1l@users.noreply.github.com>
2022-04-25 11:05:51 +0900

    Time: 2020 ms (44.03%), Space: 27.4 MB (73.18%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
@@ -2,16 +2,12 @@ class Solution:
     def maximumCandies(self, candies: List[int], k: int) -> int:
 
         l = 1  ; r =  max(candies) 
-
         while l <= r: 
 
             m = (l+r) >>1 
-
             if	sum( can//m  for can in candies ) >= k:
-
                 l = m+1
             else:
                 r = m-1 
-
         return r 
 
