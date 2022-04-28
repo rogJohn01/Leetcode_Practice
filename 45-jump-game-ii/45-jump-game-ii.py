019ad5bca8a352f6d1a9d4d5ab9a6c2f7c373fe9
john1l <94162057+john1l@users.noreply.github.com>
2022-04-28 10:59:55 +0900

    Time: 17463 ms (5.00%), Space: 15.9 MB (8.59%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -2,11 +2,10 @@ class Solution:
     def jump(self, nums: List[int]) -> int:
 
         nl = len(nums)
-        dp = [0]*10000
+        dp = [0]*100000
 
         for c in range(nl-1): 
             for i in range(1, nums[c]+1):
-                if c+i <=(nl-1):
                     if dp[c+i] !=0:
                         dp[c+i] = min(dp[c+i],dp[c]+1)
                     else:
