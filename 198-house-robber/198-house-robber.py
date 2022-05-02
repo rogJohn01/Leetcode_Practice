498ee23db1160775a597cc3589b7fca34fe07e74
john1l <94162057+john1l@users.noreply.github.com>
2022-05-02 09:57:54 +0900

    Time: 58 ms (17.58%), Space: 13.9 MB (21.69%) - LeetHub

---
+++ 198-house-robber/198-house-robber.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        if len(nums) == 1:
+            return nums[0]
+        
+        bounty = [None] * len(nums)
+        bounty[0] = nums[0]
+        bounty[1] = max(nums[0],nums[1])
+        
+        for i in range(2,len(nums)):
+            bounty[i] = max(bounty[i-2]+nums[i], bounty[i-1])
+        
+        return max(bounty)
