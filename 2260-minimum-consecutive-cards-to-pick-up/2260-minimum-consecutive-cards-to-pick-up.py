06cf5b1bc6450bfa5eec23f3fc14a9fd7d1edfa8
john1l <94162057+john1l@users.noreply.github.com>
2022-05-11 15:21:46 +0900

    Time: 288 ms (96.12%), Space: 79.5 MB (96.78%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.py
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.py
@@ -1,12 +1,15 @@
-class Solution:
-    def minimumCardPickup(self, cards: List[int]) -> int:
-        minPick = float('inf')
-        seen = {}
-        for i, n in enumerate(cards):
-            if n in seen:
-                if i - seen[n] + 1 < minPick:
-                    minPick = i - seen[n] + 1
-            seen[n] = i
-        if minPick == float('inf'):
-            return -1
-        return minPick
+class Solution {
+public:
+    int minimumCardPickup(vector<int>& cards) {
+        
+        int ans = INT_MAX , last[1000001] ={} ; 
+        for(int i=0; i<cards.size(); i++){
+            if( last[cards[i]]) {
+                ans = min( ans , i- last[cards[i]]+2 );
+            }
+            last[cards[i]] = i+1 ;
+        }
+        return  ans == INT_MAX ? -1 : ans; 
+        
+    }
+};
