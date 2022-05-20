68dec122490195e5ddc8ab94f7399dc774396744
john1l <94162057+john1l@users.noreply.github.com>
2022-05-20 10:57:52 +0900

    Time: 37 ms (97.27%), Space: 14 MB (74.88%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -1,25 +1,31 @@
 class Solution:
-    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
+    def uniquePathsWithObstacles(self, mat: List[List[int]]) -> int:
        
     
-    
-        m = len(grid)
-        n = len(grid[0])
-
-        if grid[0][0] ==1:
+        if mat[0][0] ==1:
             return 0 
 
-        grid[0][0] = 1
+        R = len(mat) ; C = len(mat[0]) 
+        wall = False 
+        for r in range(R):
+            if mat[r][0] ==1 or wall:
+                wall = True 
+                mat[r][0] = 0
+            else:
+                mat[r][0] =1 
+        wall = False 
+        for c in range(1,C):
+            if mat[0][c] ==1 or wall:
+                wall = True 
+                mat[0][c] = 0 
+            else: 
+                mat[0][c] = 1 
 
-        for i in range(1,m):
-            grid[i][0] = int(grid[i][0] ==0 and grid[i-1][0] ==1 )
-        for j in range(1,n):
-            grid[0][j] = int(grid[0][j] == 0 and grid[0][j-1]==1)
+        for r in range(1,R):
+            for c in range(1,C):
+                if mat[r][c] == 0:
+                    mat[r][c] = mat[r-1][c] + mat[r][c-1] 
+                else: # mat[r][c] ==1 
+                    mat[r][c] = 0
+        return mat[R-1][C-1] 
 
-        for i in range(1,m):
-            for j in range(1,n):
-                if grid[i][j] ==0:
-                    grid[i][j] = grid[i-1][j] + grid[i][j-1]
-                else:
-                    grid[i][j] =0 
-        return grid[m-1][n-1]
