af523c4a9b2055616c2ddb0eebd5e1a37d037115
john1l <94162057+john1l@users.noreply.github.com>
2022-06-18 12:11:22 +0900

    Time: 5402 ms (74.21%), Space: 38.1 MB (73.41%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -1,30 +1,22 @@
 class Solution:
     def minimumObstacles(self, mat: List[List[int]]) -> int:
-        
-        R = len(mat) ; C = len(mat[0])
-        visit =set()
-        visit.add((0,0))
-        q = deque()
-        q.append([0,0, 0] )
-        minv = float('inf')
-        while q:
-            for _ in range(len(q)):
-                x,y, w = q.popleft()
+        import heapq as hq 
+        inf = float('inf') 
+        R = len(mat) ; C = len(mat[0]) 
+        dist = [[inf]*C for _ in range(R) ] 
+        hp = [ ( mat[0][0] , 0, 0 ) ] 
+        dist[0][0] = mat[0][0] 
 
-                if [x,y] == [R-1,C-1]:
-                    if w < minv:
-                        minv = w
+        while hp: 
 
-                tmp= []
-                for a,b in [(1,0),(-1,0),(0,1),(0,-1)]:
-                    nx, ny =  x+a , y+b
-                    if 	0<= nx < R and 0 <= ny <C:
-                        if (nx,ny) not in visit:
-                            if mat[nx][ny] ==1:
-                                q.append([nx,ny,w+1])
-                            else:
-                                q.appendleft([nx,ny, w] )
-                            visit.add((nx,ny))
+            w , x, y = hq.heappop(hp)
+            if [x,y] == [R-1,C-1]:
+                return w 
+            for a,b in [(1,0),(-1,0),(0,1),(0,-1)]:  
+                nx, ny =  x+a , y+b 
+                if 	0<= nx < R and 0 <= ny <C: 
+                    if mat[nx][ny] + w < dist[nx][ny]: 
+                        dist[nx][ny] = mat[nx][ny] + w 
+                        hq.heappush(hp , (dist[nx][ny] , nx,ny ))
 
-        return minv
 
