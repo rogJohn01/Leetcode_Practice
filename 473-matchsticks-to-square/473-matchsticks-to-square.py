968fc7625e5439ba880cb9269855a6cf18ba02fb
존 <94162057+john1l@users.noreply.github.com>
2022-07-12 13:01:29 +0900

    Time: 3008 ms (48.35%), Space: 64.9 MB (6.78%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.py
+++ 473-matchsticks-to-square/473-matchsticks-to-square.py
@@ -1,18 +1,20 @@
 class Solution:
-    def makesquare(self, nums: List[int]) -> bool:
+    def makesquare(self, msk: List[int]) -> bool:
         
-            N = len(nums)
-            basket, rem = divmod(sum(nums), 4)
-            if rem or max(nums) > basket: return False
+           
+
+            N = len(msk)
+            box, rem = divmod(sum(msk), 4)
+            if rem or max(msk) > box: return False
 
             @lru_cache(None)
             def dfs(mask):
                 if mask == 0: return 0
                 for j in range(N):
                     if mask & 1<<j:
-                        neib = dfs(mask ^ 1<<j)
-                        if neib >= 0 and neib + nums[j] <= basket:
-                            return (neib + nums[j]) % basket
+                        tmp = dfs(mask ^ 1<<j)
+                        if tmp >= 0 and tmp + msk[j] <= box:
+                            return (tmp + msk[j]) % box
                 return -1
 
             return dfs((1<<N) - 1) == 0
