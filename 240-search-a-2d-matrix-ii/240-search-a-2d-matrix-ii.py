b4440879f1956ba1322570640c6932c849eca740
존 <94162057+john1l@users.noreply.github.com>
2022-07-24 16:59:14 +0900

    Time: 352 ms (11.71%), Space: 20.3 MB (84.45%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -1,9 +1,9 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], tar: int) -> bool:
         
-        import bisect as bi 
         for row in matrix: 
-            ix = bi.bisect_left(row, tar)
-            if ix <len(row) and row[ix] == tar: return True 
+            ix = bisect.bisect_left(row, tar)
+            if ix >=len(row): continue 
+            if row[ix] == tar: return True 
         return False  
             
