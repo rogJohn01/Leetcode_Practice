a6454a00268efc9116418a6f14af9debb4b3fd18
존 <94162057+john1l@users.noreply.github.com>
2022-07-29 11:55:52 +0900

    Time: 37 ms (87.02%), Space: 13.9 MB (76.64%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
+        
+
+        ans = [] 
+        for word in words: 
+
+            dic = {} 
+            rdic = {} 
+            jp = False 
+            for i,p in enumerate(pattern): 
+
+
+                if word[i] in dic.values() and rdic[word[i]] !=p:
+                    jp = True 
+                    break 
+
+                if p  in dic: 
+                    if  dic[p] != word[i]:
+                        jp = True 
+                        break 
+                else: 
+                    dic[p] = word[i] 
+                    rdic[word[i]] = p
+            if not jp:
+                ans.append(word) 
+
+        return ans 
+
