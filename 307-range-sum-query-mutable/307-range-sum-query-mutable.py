c0d983800399f01dbcc6e183b43e78b5446e441b
존 <94162057+john1l@users.noreply.github.com>
2022-07-31 21:25:38 +0900

    Time: 2792 ms (61.68%), Space: 30.9 MB (81.13%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.py
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.py
@@ -2,17 +2,17 @@ class BIT:
     def __init__(self, size):
         self.bit = [0] * (size + 1)
 
-    def getSum(self, idx):  # Get sum in range [1..idx], 1-based indexing
+    def getSum(self, idx): 
         s = 0
         while idx > 0:
             s += self.bit[idx]
             idx -= idx & (-idx)
         return s
 
-    def getSumRange(self, left, right):  # left, right inclusive, 1-based indexing
+    def getSumRange(self, left, right):  
         return self.getSum(right) - self.getSum(left - 1)
 
-    def addValue(self, idx, val):  # 1-based indexing
+    def addValue(self, idx, val):  
         while idx < len(self.bit):
             self.bit[idx] += val
             idx += idx & (-idx)
@@ -26,9 +26,9 @@ class NumArray:
             self.bit.addValue(i+1, v)
 
     def update(self, index: int, val: int) -> None:
-        diff = val - self.nums[index]  # get diff amount of `val` compared to current value
-        self.bit.addValue(index+1, diff)  # add this `diff` amount at index `index+1` of BIT, plus 1 because in BIT it's 1-based indexing
-        self.nums[index] = val # update latest value of `nums[index]`
-
+        diff = val - self.nums[index]  
+        self.bit.addValue(index+1, diff)  
+        self.nums[index] = val
+        
     def sumRange(self, left: int, right: int) -> int:
         return self.bit.getSumRange(left+1, right+1)
