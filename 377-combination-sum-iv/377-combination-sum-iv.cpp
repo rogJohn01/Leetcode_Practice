08190cb99936548c68017562eeafab959b5a9f94
존 <94162057+john1l@users.noreply.github.com>
2022-08-05 15:29:44 +0900

    Time: 3 ms (62.90%), Space: 6.8 MB (13.45%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int tar) {
+        vector<int> dp(tar+1 , -1) ;
+        return dfs(nums , tar , dp) ; 
+    }
+    
+
+    int dfs(vector<int>& nums , int tar , vector<int>& dp){
+
+        if(tar ==0) return 1 ; 
+        if( dp[tar] != -1) return dp[tar] ; 
+
+        dp[tar] = 0 ; 
+
+        for(auto& n:nums){
+            if( tar- n >=0){
+                dp[tar] +=  dfs(nums, tar-n , dp); 
+            }}
+        return dp[tar ] ; 
+    }
+
+
+    
+};
