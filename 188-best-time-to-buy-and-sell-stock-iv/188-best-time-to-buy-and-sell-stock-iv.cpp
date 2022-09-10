ee0a87e9b3f9c3f14f1d74390df351fe72b26260
존 <94162057+john1l@users.noreply.github.com>
2022-09-10 22:36:37 +0900

    Time: 8 ms (87.87%), Space: 10.8 MB (94.31%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        
+        
+    int len = prices.size(); 
+    if( len<2) return 0 ; 
+    if( k >len/2) { 
+        int ans = 0; 
+        for( int i=1; i<len; ++i){
+            ans+= max(prices[i] -prices[i-1],0); 
+        }
+        return ans ; 
+    }
+    int hold[k+1]; 
+    int rel[k+1] ; 
+    for( int i=0; i<=k ; ++i){
+        hold[i] = INT_MIN; 
+        rel[i] = 0 ; 
+    }
+    int cur ; 
+    for(int i=0; i<len; ++i){
+        cur = prices[i] ; 
+        for(int j=k; j>0 ; --j){
+            rel[j] = max(rel[j], hold[j]+cur); 
+            hold[j] = max(hold[j] , rel[j-1] - cur);
+        }
+    }
+    return rel[k] ; 
+    }
+};
+
+
+    
