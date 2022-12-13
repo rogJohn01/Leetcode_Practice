415ec3075a2f30bf5d9b0698d8011d1495ce549c
존 <94162057+rogJohn01@users.noreply.github.com>
2022-12-13 23:56:08 +0900

    Time: 278 ms (64.14%), Space: 14.7 MB (91.12%) - LeetHub

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minFallingPathSum(self, mat: List[List[int]]) -> int:
+        
+        if len(mat) ==1:
+            return mat[-1][-1]
+
+        R = len(mat) ; C = len(mat[0]) 
+        dp = [  [0]*C for _ in range(R)  ] 
+        dp[0] = mat[0] 
+
+        for r in range(1,R):
+            for c in range(C):
+                if c ==0:
+                    dp[r][c] = mat[r][c] + min( dp[r-1][c] , dp[r-1][c+1] ) 
+                elif c== C-1:
+                    dp[r][c] = mat[r][c] + min( dp[r-1][c-1] , dp[r-1][c] ) 
+                else:
+                    dp[r][c] = mat[r][c] + min( dp[r-1][c-1] , dp[r-1][c] , dp[r-1][c+1] ) 
+        
+        return min(dp[R-1])
+
