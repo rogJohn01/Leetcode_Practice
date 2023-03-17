be1abc869d4e390cc138e27ebaeb1eb883901870
존 <94162057+rogJohn01@users.noreply.github.com>
2023-03-17 18:34:02 +0900

    Time: 172 ms (71.38%), Space: 31.6 MB (52.55%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,33 +1,44 @@
 
+
+class Tnode:
+    def __init__(self):
+        self.children = {} 
+        self.isWord = False 
+
+
+
+
 class Trie:
 
     def __init__(self):
-        self.children = defaultdict(Trie) # recursive order 
-        self.isWord = False 
+        self.root = Tnode() 
 
     def insert(self, word: str) -> None:
-        cur = self 
-        for c in word: 
-            cur = cur.children[c]
-        cur.isWord = True 
+        node  = self.root 
+        for w in word:
+            if w not in node.children:
+                node.children[w] = Tnode()
+            node = node.children[w]
+        node.isWord = True 
+    
+
 
     def search(self, word: str) -> bool:
-        cur = self 
-        for c in word: 
-            if c not in cur.children: 
+        node = self.root 
+        for w in word: 
+            if w not in node.children: 
                 return False 
-            cur = cur.children[c]
-        return cur.isWord 
+            node = node.children[w]
+        return node.isWord 
+
 
     def startsWith(self, prefix: str) -> bool:
-
-        cur = self 
-        for c in prefix: 
-            if c not in cur.children:
+        node = self.root 
+        for w in prefix: 
+            if w not in node.children:
                 return False 
-            cur = cur.children[c]
+            node = node.children[w]
         return True 
-
 
 
 
