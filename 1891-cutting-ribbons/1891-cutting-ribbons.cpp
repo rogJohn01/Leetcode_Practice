47f1b2295e9cfd4e8156a0e1f887b40ebcde104f
존 <94162057+rogJohn01@users.noreply.github.com>
2023-05-02 22:01:14 +0900

    Time: 217 ms (51.69%), Space: 92.4 MB (86.44%) - LeetHub

--- 1891-cutting-ribbons/1891-cutting-ribbons.cpp
+++ 1891-cutting-ribbons/1891-cutting-ribbons.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    int maxLength(vector<int>& ribbons, int k) {
-     
-        int cnt ; 
-        int l = 1; 
-        int r = *max_element(ribbons.begin() , ribbons.end()) ;
-        
-
-        while(l<=r){
-
-            int m = l+ (r-l)/2 ; 
-            cnt = 0 ; 
-            for(auto r:ribbons){
-                cnt +=  r/m  ;
-            }
-
-            if(cnt < k){
-                r = m-1; 
-            }else {
-                l = m+1; 
-            }
-           
-
-        }return r ; 
+    int maxLength(vector<int>& ribs, int k) {
+    int l = 0, r = accumulate(begin(ribs), end(ribs), 0l) / k;
+    while (l < r) {
+        int m = (l + r + 1) / 2, obtain = 0;
+        for (int i = 0; i < ribs.size() && obtain < k; ++i)
+            obtain += ribs[i] / m;
+        if (obtain < k)
+            r = m - 1;
+        else
+            l = m;
     }
+    return l;
+}
 };
