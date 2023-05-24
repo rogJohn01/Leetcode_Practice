9dcc92ea3b80aad72c1be1822f45fb611703ce6f
존 <94162057+rogJohn01@users.noreply.github.com>
2023-05-24 21:18:43 +0900

    Time: 1869 ms (67.43%), Space: 0B (100.00%) - LeetHub

--- 1083-sales-analysis-ii/1083-sales-analysis-ii.sql
+++ 1083-sales-analysis-ii/1083-sales-analysis-ii.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
 
-select s.buyer_id from sales s left join product p
-on s.product_id = p.product_id
-group by s.buyer_id 
-having sum(p.product_name = 'S8') >0 and sum(p.product_name ='iphone') =0 
+
+select buyer_id from sales s join product p 
+on s.product_id = p.product_id 
+group by buyer_id 
+having sum(p.product_name = 's8' ) > 0 and sum(p.product_name ='iphone' ) = 0 
