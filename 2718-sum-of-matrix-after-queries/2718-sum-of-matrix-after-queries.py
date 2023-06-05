3251776ddbe8311932814ea71861b65fa65c49dd
존 <94162057+rogJohn01@users.noreply.github.com>
2023-06-05 17:28:28 +0900

    Time: 1952 ms (33.33%), Space: 38.7 MB (16.67%) - LeetHub

--- 2718-sum-of-matrix-after-queries/2718-sum-of-matrix-after-queries.py
+++ 2718-sum-of-matrix-after-queries/2718-sum-of-matrix-after-queries.py
@@ -1,18 +1,19 @@
 class Solution:
     def matrixSumQueries(self, n: int, q: List[List[int]]) -> int:
-        
+
+
         q.reverse()
+
         rows = set()
         cols = set()
-        ans = 0
-        
-        for t, i, v in q:
-            if t == 0 and i not in cols:
-                ans += v * (n - len(rows))
-                cols.add(i)
-            
-            if t == 1 and i not in rows:
-                ans += v * (n - len(cols))
-                rows.add(i)
-                
-        return ans
+        ans =0 
+
+        for t, ix , v in q: 
+
+            if t==0 and ix not in rows: 
+                ans += v*(n-len(cols))
+                rows.add(ix)
+            if t==1 and ix not in cols: 
+                ans += v*(n-len(rows))
+                cols.add(ix)
+        return ans 
